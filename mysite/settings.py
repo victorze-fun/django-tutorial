6bc788ff9bd75eff63e265ecb622f5b5169a31d9
Victor <victorze@protonmail.com>
2021-09-15 16:20:52 -0500

    Add public interface

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ck&6(=%u&f@4=69q&fl-*7wunhyz-+1c3cb+!&*k5yz+$%%m^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
